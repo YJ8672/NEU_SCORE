2e11b2430559acdd10c44cae4ddc22f1ca369fdd
UPDATE <leftongyu@gmail.com>
2023-06-29 18:49:05 +0000

    2023-06-29 18:49:05

--- score_num.js
+++ score_num.js
@@ -1,1 +1,1 @@
-async function getData(){const course = "61_新闻摄影（在线式）_自然语言处理（双语）_医学成像技术与医学图像处理_智能医学算法设计_深度学习_形势与政策(3)";return course;}getData().then((course) => {console.log(course);return course;}); 
+async function getData(){const course = "65_生物系统测量（英语）_健康人体物联网（双语）_形势与政策（4）_形势与政策(3)";return course;}getData().then((course) => {console.log(course);return course;}); 
